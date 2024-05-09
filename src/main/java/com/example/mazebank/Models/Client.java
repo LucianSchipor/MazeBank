ea942e905bd296d4f3d65db6463eb4701304f942
Lucian <schiporlucian20021@gmail.com>
2024-05-09 10:17:58 +0300

    [api] - created client class

--- src/main/java/com/example/mazebank/Models/Client.java
+++ src/main/java/com/example/mazebank/Models/Client.java
@@ -5,29 +5,26 @@ import javafx.beans.property.*;
 import java.time.LocalDate;
 
 public class Client {
-//    private final StringProperty firstName;
-//    private final StringProperty lastName;
-//    private final StringProperty payeeAddress;
-//    private final ObjectProperty<Account> checkingAccount;
-//    private final ObjectProperty<Account> savingsAccount;
-//    private final ObjectProperty<LocalDate> dateCreated;
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty payeeAddress;
+    private final ObjectProperty<Account> checkingAccount;
+    private final ObjectProperty<Account> savingsAccount;
+    private final ObjectProperty<LocalDate> dateCreated;
 
-//    public Client(StringProperty firstName, StringProperty lastName, StringProperty payeeAddress, ObjectProperty<Account> checkingAccount, ObjectProperty<Account> savingsAccount, ObjectProperty<LocalDate> dateCreated) {
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.payeeAddress = payeeAddress;
-//        this.checkingAccount = checkingAccount;
-//        this.savingsAccount = savingsAccount;
-//        this.dateCreated = dateCreated;
-//    }
+    public Client (String fName, String lName, String pAddress, Account cAccount, Account sAccount, LocalDate date) {
+        this.firstName = new SimpleStringProperty(this, "FirstName", fName);
+        this.lastName = new SimpleStringProperty(this, "LastName", lName);
+        this.payeeAddress = new SimpleStringProperty(this, "Payee Address", pAddress);
+        this.checkingAccount = new SimpleObjectProperty<>(this, "Checking Account", cAccount);
+        this.savingsAccount = new SimpleObjectProperty<>(this, "Savings Account", sAccount);
+        this.dateCreated = new SimpleObjectProperty<>(this, "Date", date);
+    }
 
-//    public Client (String fName, String lName, String pAddress, Account cAccount, Account sAccount, LocalDate date) {
-//        this.firstName = new SimpleStringProperty((this, "FirstName", fName));
-//        this.lastName = new SimpleStringProperty((this, "LastName", lName));
-//        this.payeeAddress = new SimpleStringProperty((this, "Payee Address", pAddress));
-//        this.checkingAccount = new SimpleObjectProperty<>(this, "Checking Account", cAccount);
-//        this.savingsAccount = new SimpleObjectProperty<>(this, "Savings Account", sAccount);
-//        this.dateCreated = new SimpleObjectProperty<>(this, "Date", date);
-//
-//    }
+    public StringProperty firstNameProperty() {return firstName;}
+    public StringProperty lastNameProperty() {return lastName;}
+    public StringProperty pAdressProperty(){return payeeAddress;}
+    public ObjectProperty<Account> checkingAccountProperty(){return checkingAccount;}
+    public ObjectProperty<Account> savingsAccountProperty(){return savingsAccount;}
+    public ObjectProperty<LocalDate> dateCreatedProperty() {return dateCreated;}
 }
