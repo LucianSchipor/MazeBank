6cc567f858d935d9d9161265fe9533fb2d188a6d
Lucian <schiporlucian20021@gmail.com>
2024-11-20 12:25:51 +0200

    [api] - refactor

--- src/main/java/com/example/mazebank/Application.java
+++ src/main/java/com/example/mazebank/Application.java
@@ -6,7 +6,12 @@ import javafx.stage.Stage;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage){
+        System.out.println("Java version - " + System.getProperty("java.version"));
+        System.out.println("JavaFX version - " +  System.getProperty("javafx.version"));
+
         Model.getInstance().getViewFactory().showLoginWindow();
+
+
     }
 }
 
